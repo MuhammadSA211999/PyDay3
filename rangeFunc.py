2d5208866ed179adca21caedc2ad8ecf23607060
Muhammad SA <MuhammadSA211999>
2023-07-22 21:17:19 +0600

    range fanvtion with list

---
+++ rangeFunc.py
@@ -0,0 +1,13 @@
+# range function diye ekti nirdisto songkha porzonto conditional number print kora zay.
+# ** range function tike list e convert kora zay.
+
+numbers10 = range(10)
+# range(0,10)
+numbers10 = list(range(10))
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(numbers10)
+
+numbers5to10 = list(range(1, 99, 3))
+# range e 1 er value 1, 99 er value 98, 3 er value 2
+# [5,.....,51]
+print(numbers5to10)
